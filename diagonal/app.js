225a1e716ae29d59ca72ddfb4325247494a7b627
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-20 11:14:38 +0200

    diagonal

---
+++ diagonal/app.js
@@ -0,0 +1,17 @@
+var canvas = document.getElementById('myCanvas');
+var context = canvas.getContext('2d');
+
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+context.beginPath();
+context.moveTo(canvasWidth - canvasWidth, canvasHeight - canvasHeight);
+context.lineTo(canvasWidth, canvasHeight);
+context.strokeStyle = 'red';
+context.stroke();
+
+context.beginPath();
+context.moveTo(canvasWidth - canvasWidth, canvasHeight);
+context.lineTo(canvasWidth, canvasHeight - canvasHeight);
+context.strokeStyle = 'red';
+context.stroke();
